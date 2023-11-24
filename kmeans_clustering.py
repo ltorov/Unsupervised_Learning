5b2ae5c58191e65dc9f8075ea334a2f3d4c3c4f5
Luisa Toro Villegas <ltorov@eafit.edu.co>
2023-11-23 20:05:14 -0500

    :)

---
+++ kmeans_clustering.py
@@ -0,0 +1,125 @@
+import numpy as np
+import random
+from distances import distance, sort_distances
+
+class KMeansClustering:
+    def __init__(self, k: int, centers: np.array = None, max_iterations: int = 100, epsilon: float = 0.45, metric = 2):
+        """
+        Initialize the KMeansClustering class.
+
+        Args:
+            k (int): Number of clusters to create.
+            centers (np.array): Initial cluster centers. Default is None.
+            max_iterations (int): Maximum number of iterations for clustering. Default is 100.
+            epsilon (float): Convergence threshold for stopping the iterations. Default is 0.1.
+        """
+        self.k = k
+        self.centers = centers if centers != None else []
+        self.max_iterations = max_iterations
+        self.epsilon = epsilon
+        self.clusters = {}
+        self.metric = metric
+
+    def dissimilarity_measure(self, data: np.array) -> (float, np.array):
+        """
+        Calculate the dissimilarity measure of the clustering.
+
+        Args:
+            data (np.array): Data points to cluster.
+
+        Returns:
+            J (float): Dissimilarity measure of the clustering.
+            U (np.array): Membership matrix.
+        """
+        J = 0
+        U = self.membership_matrix(data)
+        for i, ci in enumerate(self.centers):
+            for k, xk in enumerate(data):
+                J += U[i][k] * np.linalg.norm(xk - ci, ord = self.metric)
+
+        return J, U
+
+    def membership_matrix(self, data) -> np.array:
+        """
+        Calculate the membership matrix for the data.
+
+        Args:
+            data (np.array): Data points to cluster.
+
+        Returns:
+            U (np.array): Membership matrix.
+        """
+        U = np.zeros((self.k, len(data)))
+
+        for i, ci in enumerate(self.centers):
+            for j, xj in enumerate(data):
+                if i == j:
+                    continue
+                uij = 1
+                for ck in self.centers:
+                    if not (np.linalg.norm(xj - ci, ord = self.metric) <= np.linalg.norm(xj - ck, ord = self.metric)):
+                        uij = 0
+                U[i, j] = uij
+        return U
+    
+    def update_centers(self, data) -> np.array:
+        new_centers = np.zeros((len(self.clusters.keys()), data.shape[1]))
+        for i, key in enumerate(self.clusters.keys()):
+            new_centers[i,:] = 1/len(self.clusters[key])* np.sum(data[self.clusters[key]], axis =0)
+        self.centers = new_centers
+
+    def update_clusters(self, U):
+        clusters = {}
+        for i, row in enumerate(U):
+                indices = np.where(row == 1)[0]
+                if indices.size > 0:
+                    clusters[i] = indices.tolist()
+        self.clusters = clusters
+        
+
+
+    def cluster(self, data: np.array) -> dict:
+        """
+        Cluster the data using the K-Means algorithm.
+
+        Args:
+            data (np.array): Data points to cluster.
+
+        Returns:
+            clusters (dict): Dictionary of clusters, where keys are cluster indices, and values are lists of data indices.
+        """
+        dissimilarity_measures = []
+        iter = 0
+        if self.centers == []:
+            centers_indexes = []
+            centers_indexes.append(np.random.choice(np.arange(len(data)), size = 1)[0])
+            for k_i in range(self.k-1):
+                distances = distance(data, data[centers_indexes], metric = "euclidean")
+                nearest_distances = np.min(distances, axis=0)**2
+                selected_index = random.choices(range(len(nearest_distances)), weights=nearest_distances)[0]
+                centers_indexes.append(selected_index)
+            self.centers = data[centers_indexes]
+
+        J, U = self.dissimilarity_measure(data)
+        dissimilarity_measures.append(J)
+        self.update_clusters(U)
+        self.update_centers(data)
+        while True and iter < self.max_iterations:
+
+            J, U = self.dissimilarity_measure(data)
+            if (dissimilarity_measures[-1] - J) < self.epsilon:
+                break
+            else:
+                dissimilarity_measures.append(J)
+                self.update_clusters(U)
+                self.update_centers(data)
+                iter += 1
+
+        new_clusters = {}
+
+        for cluster_center, points in self.clusters.items():
+            cluster_name = f'cluster {cluster_center}'
+            center = data[cluster_center].tolist()
+            new_clusters[cluster_name] = {'center': np.array(center), 'points': np.array(points)}
+
+        return new_clusters
---
+++ plotting.py
@@ -0,0 +1,217 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import warnings
+from scipy.interpolate import griddata
+from babyplots import Babyplot
+
+def immersive_scatter(data, clusters, axis= [0,1,2], colors = "YlGnBu", xlabel = "X", ylabel = "Y", zlabel = "Z"):
+    points = [clusters[cluster]['points'] for cluster in clusters.keys()]
+    targets = [i for n in range(len(data)) for i in range(len(points)) if n in points[i]]
+    data = data[:, axis]
+    
+    # create the babyplots visualization
+    bp = Babyplot()
+    bp.add_plot(data, "shapeCloud", "categories", targets, {"shape": "sphere",
+                                                                    "colorScale": colors,
+                                                                    "showAxes": [True, True, True],
+                                                                    "axisLabels": [xlabel, ylabel, zlabel]})
+    # show the visualization
+    return bp
+
+# Function to save the plot to the "results" folder
+def save_plot(plt, title, folder="results", file_format="png"):
+    # Create the "results" folder if it doesn't exist
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    # Generate the filename from the title
+    filename = os.path.join(folder, title.replace(" ", "_") + "." + file_format)
+
+    # Save the plot with the generated filename
+    plt.savefig(filename)
+
+def scatter(data, title = "Scatter plot for data"):
+    # Create a 3D scatter plot for each combination of dimensions
+    fig = plt.figure(figsize=(12, 8))
+
+    # Plot 1: Dimensions 0, 1, and 2
+    ax1 = fig.add_subplot(221, projection='3d')
+    ax1.scatter(data[:, 0], data[:, 1], data[:, 2])
+    ax1.set_xlabel('Dimension 0')
+    ax1.set_ylabel('Dimension 1')
+    ax1.set_zlabel('Dimension 2')
+    ax1.set_title('3D Scatter Plot: Dimensions 0, 1, and 2')
+
+    # Plot 2: Dimensions 0, 1, and 3
+    ax2 = fig.add_subplot(222, projection='3d')
+    ax2.scatter(data[:, 0], data[:, 1], data[:, 3])
+    ax2.set_xlabel('Dimension 0')
+    ax2.set_ylabel('Dimension 1')
+    ax2.set_zlabel('Dimension 3')
+    ax2.set_title('3D Scatter Plot: Dimensions 0, 1, and 3')
+
+    # Plot 3: Dimensions 0, 2, and 3
+    ax3 = fig.add_subplot(223, projection='3d')
+    ax3.scatter(data[:, 0], data[:, 2], data[:, 3])
+    ax3.set_xlabel('Dimension 0')
+    ax3.set_ylabel('Dimension 2')
+    ax3.set_zlabel('Dimension 3')
+    ax3.set_title('3D Scatter Plot: Dimensions 0, 2, and 3')
+
+    # Plot 4: Dimensions 1, 2, and 3
+    ax4 = fig.add_subplot(224, projection='3d')
+    ax4.scatter(data[:, 1], data[:, 2], data[:, 3])
+    ax4.set_xlabel('Dimension 1')
+    ax4.set_ylabel('Dimension 2')
+    ax4.set_zlabel('Dimension 3')
+    ax4.set_title('3D Scatter Plot: Dimensions 1, 2, and 3')
+
+    # Add title
+    plt.title(title)
+
+    plt.tight_layout()
+    
+    # Save plot to results folder
+    save_plot(plt, title)
+
+def heatmap(D, title = "Heatmap of Distance Matrix", sorted = False, colors ="YlGnBu"):
+    if sorted:
+        # Flatten the matrix into a 1D array, preserving the original indices
+        flattened_matrix = D.flatten()
+        indices = np.arange(len(flattened_matrix))
+
+        # Sort the flattened matrix and corresponding indices
+        sorted_indices = np.argsort(flattened_matrix)
+        sorted_matrix = flattened_matrix[sorted_indices]
+        sorted_original_indices = indices[sorted_indices]
+
+        # Reshape the sorted 1D array back into a 2D matrix
+        D = sorted_matrix.reshape(D.shape)
+    # Create a heatmap using seaborn
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(D, cmap=colors)
+
+    # Add labels and title
+    plt.title(title)
+    plt.xlabel("D")
+    plt.ylabel("D")
+
+    # Save plot to results folder
+    save_plot(plt, title)
+
+def surface(D, title= 'Surface plot of Distance Matrix', colors ="YlGnBu"):
+    # Create x, y coordinates based on the shape of D
+    x = np.arange(D.shape[0])
+    y = np.arange(D.shape[1])
+    x, y = np.meshgrid(x, y)
+
+    # Create a figure and a 3D axis
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Flatten the matrix D for the z-values
+    z = D.flatten()
+
+    # Plot the 3D surface
+    surf = ax.plot_surface(x, y, D, cmap=colors)
+
+    # Set labels for the axes
+    ax.set_xlabel('X Axis')
+    ax.set_ylabel('Y Axis')
+    ax.set_zlabel('Z Axis')
+
+    # Add a color bar
+    fig.colorbar(surf, ax=ax, shrink=0.5, aspect=10)
+
+    # Add title
+    plt.title(title)
+
+    # Save plot to results folder
+    save_plot(plt, title)
+
+
+
+
+def scatter_clusters(data, clusters, title = "Clusters", palette="YlGnBu", x_label='X', y_label='Y',
+                             z_label='Z', axis = [0,1,2], return_figure=False):
+    """
+    Create a 3D scatter plot of data points with different clusters using Matplotlib.
+
+    Parameters:
+        data (numpy.ndarray): Data points with three columns (x, y, z).
+        clusters (dict): A dictionary where keys are cluster labels and values are lists of indices in each cluster.
+        palette (str): Color palette for clusters.
+        x_label (str): Label for the x-axis.
+        y_label (str): Label for the y-axis.
+        z_label (str): Label for the z-axis.
+        figsize (tuple): Figure size (width, height) for the plot.
+        return_figure (bool): Whether to return the Matplotlib figure.
+
+    Returns:
+        matplotlib.figure.Figure (optional): The Matplotlib figure if return_figure is True, else None.
+    """
+    # Suppress warnings
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    warnings.filterwarnings("ignore", category=UserWarning)
+
+    figsize=(10, 6)
+    fig = plt.figure(figsize=figsize)
+    
+
+    colors = sns.color_palette(palette, len(clusters))
+    points = [clusters[cluster]['points'] for cluster in clusters.keys()]
+
+    if data.shape[1]>2:
+        ax = fig.add_subplot(111, projection='3d')
+
+        for id_color, point in enumerate(points):
+
+            ax.scatter(data[point][:, axis[0]], data[point][:, axis[1]], data[point][:, axis[2]], color=colors[id_color], marker='o', label=id_color)
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+        ax.set_zlabel(z_label)
+    elif data.shape[1] == 2:
+        for id_color, point in enumerate(points):
+            plt.scatter(data[point][:, axis[0]], data[point][:, axis[1]], color=colors[id_color], marker='o', label=id_color)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+
+
+    if return_figure:
+        return fig
+
+    plt.title(title)
+    plt.legend()
+    plt.show()
+
+    # Restore warnings to their default behavior
+    warnings.resetwarnings()
+
+
+
+def scatter_to_surface(x, y, z, title = "Surface plot", xlabel = 'X Axis',  ylabel = 'Y Axis', zlabel = 'Z Axis', colors = "YlGnBu"):
+    # Create a 3D figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Define the grid for interpolation
+    xi, yi = np.meshgrid(np.linspace(min(x), max(x), 100), np.linspace(min(y), max(y), 100))
+
+    # Interpolate Z-values to create a surface
+    zi = griddata((x, y), z, (xi, yi), method='cubic')
+
+    # Create the surface plot
+    ax.plot_surface(xi, yi, zi, cmap=colors)
+
+    # Set labels for the axes
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_zlabel(zlabel)
+
+    # Set title
+    plt.title(title)
+
+    # Show the plot
+    plt.show()
